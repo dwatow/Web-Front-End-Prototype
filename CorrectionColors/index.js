e7da4f514456396b6bbf4e3f422415fc77fa776b
chris <dwatow@gmail.com>
2019-01-23 22:55:30 +0800

    add CorrectionColors

---
+++ CorrectionColors/index.js
@@ -0,0 +1,34 @@
+
+const patterns = [
+  'you_can_use_arrow_key_left_or_right',
+  'gray256',
+  'gray16',
+  'red',
+  'green',
+  'blue',
+  'write',
+  'black',
+]
+
+let index = 0;
+document.addEventListener('keyup', e => {
+  const code = e.keyCode;
+  switch (code) {
+    case 37: // <-
+      index -= 1;
+      if (index <= 0) index = patterns.length -1;
+      console.log(index);
+      break;
+    case 39: // ->
+      index += 1;
+      if (index >= patterns.length) index = 1;
+      console.log(index);
+      break;
+  }
+  location.hash = patterns[index];
+})
+
+window.addEventListener('hashchange', () => {
+  console.log(patterns[index], index, location.hash);
+  document.body.className = location.hash.split('#').pop();
+})
