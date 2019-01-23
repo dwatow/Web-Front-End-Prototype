a8d75d184b6e14b457b109a45dc2bc1081268b3f
chris <dwatow@gmail.com>
2019-01-23 22:59:27 +0800

    remove log for CorrectionColors

--- CorrectionColors/index.js
+++ CorrectionColors/index.js
@@ -18,18 +18,15 @@ document.addEventListener('keyup', e => {
     case 37: // <-
       index -= 1;
       if (index <= 0) index = patterns.length -1;
-      console.log(index);
       break;
     case 39: // ->
       index += 1;
       if (index >= patterns.length) index = 1;
-      console.log(index);
       break;
   }
   location.hash = patterns[index];
 })
 
 window.addEventListener('hashchange', () => {
-  console.log(patterns[index], index, location.hash);
   document.body.className = location.hash.split('#').pop();
 })
